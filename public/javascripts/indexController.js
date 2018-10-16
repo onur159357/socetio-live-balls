91fb7a2779c42efaed9a041418c31928ffcee0fa
onur159357 <onurrryilmaz@gmail.com>
2018-10-16 15:44:53 +0300

    yedek

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,27 +1,170 @@
-// let controller = (scope) => {
-//     const socket = io.connect('http://localhost:3000/'); 
-// }
-
-// document.getElementById("myController").addEventListener("load", controller());
-
 const connectionOption = {
-    reconnectionAttempts : 3,
-    reconnectionDelay : 600
+    reconnectionAttempts : 3, // 3 defa bağlanmayı densin
+    reconnectionDelay : 600 // 600 milisaniyede bir bağlanmayı densin
 
 }
+let userStatusTag = document.querySelectorAll('.list-group');
+let ballContent = document.querySelectorAll('.ball-content');
+let users;
+let container = document.querySelector("#myController");
+let theThing;
+let socketContent;
 
-let indexFactory = () => {
+let ballUserHTML = (data) => {
+    return new Promise((resolve, reject) => {
+        htmlContent = new String();
+
+        console.log(data);
+        for(key in data) {
+            htmlContent +=  `
+                <div class="ball-user" id="${data[key].id}" style = 'left: ${data[key].position.x}px; top:${data[key].position.y}px'>
+                    <div class="ball-msg">message</div>
+                    <div class="ball-user-name"> ${data[key].userName} </div>
+                </div>`;
+
+        }
+
+        resolve(htmlContent);
+
+    })
+    
+}
+
+//soket bağlantısını sağlayan function
+let indexFactory = (userName) => {
     connectSocket('http://localhost:3000/', connectionOption)
-        .then((data) => {
-            console.log(data);
-            return data;
+        .then((socket) => {
+            socketContent = socket;
+            //init() function u içerisinden gelen user name i server a yolladık
+            socket.emit('newUser', { userName });
 
+            //server dan gelen userData objesini aldık ve index.hbs ye yazdırdık.
+            socket.on('newUser', (data) => {
+                users[data.id] = data;
+                userStatusTag[0].insertAdjacentHTML('beforeend', `<li class="list-group-item">[SERVER] <b> ${data.userName} </b> Katıldı</li> `);
+
+                ballUserHTML(users).then((data) => {
+                    ballContent[0].innerHTML = data;
+                    return data;
+
+                }).then(() => {
+                    theThing = document.getElementById(`${socket.id}`);
+                    container.addEventListener("click", getClickPosition, false);
+
+                });
+                
+            });
+
+            //Çıkış yapan kullanıcı ile ilgili bilgileri servardan alıyoruz.
+            socket.on('disUser', (data) => {
+                userStatusTag[0].insertAdjacentHTML('beforeend', `<li class="list-group-item">[SERVER] <b> ${data.userName} </b>Ayrıldı</li>`);
+
+                delete users[data.id];  
+                let userBall = document.getElementById(`${data.id}`);
+                userBall.remove(userBall);
+
+            });
+
+            //Kullanıcı giriş yaptığında servardan zaten var olan kullanıcıların bilgilerini alıyoruz.
+            socket.on('initPlayer', (data) => { 
+                users = data;
+                ballUserHTML(users).then((data) => {
+                    ballContent[0].innerHTML = data;
+                    return data;
+
+                }).then(() => {
+                    theThing = document.getElementById(`${socket.id}`);
+                    container.addEventListener("click", getClickPosition, false);
+
+                });
+                
+            });
+
+            //Servardan gelen animasyon kodlarını tüm kullanıcılar için alıyoruz
+            socket.on('animate', (data) => {
+                users[data.socketId].position.x = data.x;
+                users[data.socketId].position.y = data.y;
+
+                let userBallId = document.getElementById(data.socketId);
+                    userBallId.style.top = data.y;
+                    userBallId.style.left = data.x;
+
+            })
+            
         }).catch((err) => {
             console.log(err);
             return err;
-
+                                    
         })
 
 }
 
-document.getElementById("myController").addEventListener("load", indexFactory());
+//user name girildiği zaman çalışacak function
+const init = () => {
+    const userName = prompt('please enter user name');
+    //user name girildiyse socket bağlantısını gerçekleştiriyoruz
+    if(userName)
+        indexFactory(userName)
+    else 
+        return false;
+  
+};
+
+let myControler = document.getElementById("myController");
+
+if(myControler !== null)
+    init();
+else
+    console.log('myControler yok');
+
+//Animasyon işlemleri
+function getClickPosition(e) {
+    let parentPosition = getPosition(e.currentTarget);
+    let xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
+    let yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
+    
+    theThing.style.left = xPosition + "px";
+    theThing.style.top = yPosition + "px";
+
+    console.log(xPosition, yPosition);
+    users[theThing.id].position.x = xPosition;
+    users[theThing.id].position.y = yPosition;
+    
+    socketAnimate(xPosition, yPosition);
+
+}
+
+//Bir elemanın tam konumunu elde etmek için yardımcı fonksiyon
+function getPosition(el) {
+    let xPos = 0;
+    let yPos = 0;
+    
+    while (el) {
+        if (el.tagName == "BODY") {
+            // gövde / pencere / belge ve sayfa kaydırma ile tarayıcı sorunları ile başa çıkmak
+            let xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+            let yScroll = el.scrollTop || document.documentElement.scrollTop;
+        
+            xPos += (el.offsetLeft - xScroll + el.clientLeft);
+            yPos += (el.offsetTop - yScroll + el.clientTop);
+
+        } else {
+            // diğer tüm BODY olmayan öğeler için
+            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+
+        }
+    
+        el = el.offsetParent;
+    }
+    return {
+        x: xPos,
+        y: yPos
+
+    };
+}
+
+const socketAnimate = (x, y) => {
+    socketContent.emit('animate', { x, y });
+
+}
--- src/socketApi.js
+++ src/socketApi.js
@@ -1,14 +1,59 @@
 const socketio = require('socket.io'); //sayfaya socket io yu dahil ettik
 const io = socketio(); //io değişkenine socket io nun contractor(müteahhit) ini atadık;
-const socketApi = {};
 
+const socketApi = {};
 socketApi.io = io;
+
+const users = {};
 
 //connection ile bağlantı durumunu kontorol ediyoruz.
 io.on('connection', (socket) => {
-    console.log('hey burdayım');
+    //client den gelen user name i aldık
+   socket.on('newUser', (data) => {
+       //client den gelen username, socket.id ve position u düzenleyip users array ine atadık
+       const defaultData = {
+           id : socket.id,
+           position : {
+               x : 0,
+               y : 0,
+           }
+       }
+       const userData = Object.assign(data, defaultData); // İki objeyi birleştirdik
+       users[socket.id] = userData;
+
+       //userData objesini kendisi hariç bütün kullanıcıları görebileceği şekilde client e yolladık
+       socket.broadcast.emit('newUser', userData);
+
+       //Kullanıcı giriş yaptığında diğer userlar ile ilgili bilgileri yolluyoruz
+       socket.emit('initPlayer', users);
+
+   })
+   //Çıkış yapan user ı yakalamak için disconnect i kullanıyoruz.
+   socket.on('disconnect', () => {
+        //çıkış yapan kullanıcının tüm bilgilerini disUser ile client a yolladık.
+        socket.broadcast.emit('disUser', users[socket.id]);
+
+        //users objesinden çıkan kullanıcıyı siliyoruz
+        delete users[socket.id];
+
+   })
+   //Animasyon işlemlerini halletme
+   socket.on('animate', (data)=> {
+       users[socket.id].position.x = data.x;
+       users[socket.id].position.y = data.y;
+
+       //Animasyonu tüm kullanıcılarda gönderiyoruz
+       socket.broadcast.emit('animate', {
+           socketId : socket.id,
+           x : data.x,
+           y : data.y,
+       })
+
+   })
+
 });
 
 module.exports = socketApi;
 //modül haline getirdik ve
 //bin altında www içerisine ekliyoruz
+
